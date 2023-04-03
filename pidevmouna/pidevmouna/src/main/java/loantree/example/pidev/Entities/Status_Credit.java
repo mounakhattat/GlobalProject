8aee2f0d8fcd44c456b421f73e3b70d901b40095
mouna <mouna.khattat@esprit.tn>
2023-04-03 02:27:07 +0200

    supp

--- pidevmouna/pidevmouna/src/main/java/loantree/example/pidev/Entities/Status_Credit.java
+++
@@ -1,5 +0,0 @@
-package loantree.example.pidev.Entities;
-
-public enum Status_Credit {
-    IN_PROGRESS, REFUNDED
-}
--- pidevmouna/pidevmouna/src/main/java/loantree/example/pidev/Entities/salaires_empl.java
+++
@@ -1,5 +0,0 @@
-package loantree.example.pidev.Entities;
-
-public enum salaires_empl {
-    RH,Simple_empl,director
-}
