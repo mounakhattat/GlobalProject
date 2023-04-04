174e95f4076e1cf025b12bf4fc08bf582dd476f6
mouna <mouna.khattat@esprit.tn>
2023-04-04 04:02:23 +0200

    Update EmailService.java

--- pidevmouna/pidevmouna/src/main/java/loantree/example/pidev/services/EmailService.java
+++ pidevmouna/pidevmouna/src/main/java/loantree/example/pidev/services/EmailService.java
@@ -1,25 +1,78 @@
-package loantree.example.pidev.services;
+package loantree.example.pidev.Services;
+
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 
 @Service
 public class EmailService {
 
+    @Value("${app.sendgrid.templateId}")
+    private String templateId;
     @Autowired
-    private JavaMailSender javaMailSender;
+    SendGrid sendGrid;
+    public String sendEmail(String email)  {
 
-    public void sendEmail(String to, String subject, String body) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(body, true);
-        javaMailSender.send(message);
+        try {
+            Mail mail = prepareMail(email);
+
+            Request request = new Request();
+
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+
+            request.setBody(mail.build());
+
+
+            Response response = sendGrid.api(request);
+
+            if(response!=null) {
+
+                System.out.println("response code from sendgrid"+response.getHeaders());
+
+            }
+
+        } catch (IOException e) {
+
+
+            e.printStackTrace();
+            return "error in sent mail!";
+        }
+
+        return "mail has been sent check your inbox!";
+
+    }
+
+    public Mail prepareMail(String email) {
+
+        Mail mail = new Mail();
+
+        Email fromEmail = new Email();
+
+        fromEmail.setEmail("sahraouirayen3@gmail.com");
+
+        mail.setFrom(fromEmail);
+        Email to = new Email();
+        to.setEmail(email);
+
+
+        Personalization personalization = new Personalization();
+
+        personalization.addTo(to);
+        mail.addPersonalization(personalization);
+
+        mail.setTemplateId(templateId);
+
+        return mail;
     }
 }
